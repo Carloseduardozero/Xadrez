b68426d152fe2025b964b0bdb9b1c9c0d22cccf0
Carlos Eduardo <carloseduardozero0@gmail.com>
2022-11-29 10:58:36 -0300

    melhorando o imprimir tabuleiro

--- Program.cs
+++ Program.cs
@@ -12,13 +12,9 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            
 
-            Console.WriteLine(pos);
-
-            Console.WriteLine(pos.toPosicao());
-
-           /* try
+            try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
@@ -27,13 +23,16 @@ namespace Xadrez
                 tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
                 tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
+
+                tab.ColocarPeca(new Torre(Cor.Branca, tab), new Posicao(3, 5));
+
                 Tela.imprimirTabuleiro(tab);
             }
             catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
-           */
+           
             Console.ReadLine();
 
         }
